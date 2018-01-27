3098c9e634d98ee157b4f9061e607b36660e7fc2
HaedHutner <mvvasilev@outlook.com>
2018-01-27 19:03:42 +0200

    Removed submodule glew

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <GLFW\glfw3.h>
 
 int main() {
 	std::cout << "Working!" << std::endl;
+
+	if (glfwInit()) {
+		std::cout << "GLFW Working!" << std::endl;
+	}
+
 	return 0;
 }
